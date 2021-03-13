35bb16968a5bef2089398e0bfd25eb5f8c3beede
J. Bruno Morgado <jb.morgado@gmail.com>
2021-03-13 11:07:07 +0000

    debug change selection

--- src/sidebar.js
+++ src/sidebar.js
@@ -47,22 +47,21 @@ function calcByType(widgets) {
 }
 
 function countCards(list, keyGetter) {
-  let storyPoints = 0
-  let stories = 0
+  // let storyPoints = 0
+  // let stories = 0
   const map = new Map()
   list.forEach((item) => {
     const key = keyGetter(item)
     if (key === "CARD") {
       const count = map.get(key)
-      stories = stories + 1
-      map.set("Stories", stories)
+      // stories = stories + 1
+      // map.set("Stories", stories)
       map.set(key, !count ? 1 : count + 1)
-      storyPoints = storyPoints + parseFloat(item.card.customFields[3].value)
-      map.set("Story Points", storyPoints)
+      // storyPoints = storyPoints + parseFloat(item.card.customFields[3].value)
+      // map.set("Story Points", storyPoints)
     }
   })
-  return map
-  // return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
+  return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
 }
 
 miro.onReady(() => {
