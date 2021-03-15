a154dfdf31c1a82816ff5749dde7ddf14c85921d
J. Bruno Morgado <jb.morgado@gmail.com>
2021-03-15 15:29:41 +0000

    remove extra functions

--- src/sidebar.js
+++ src/sidebar.js
@@ -46,36 +46,18 @@ function calcByType(widgets) {
   const map = new Map()
   widgets.forEach((item) => {
     if (item.type === "CARD") {
-      const status = item.card.customFields[0].value
-      const countStatus = map.get(status)
+      // const status = item.card.customFields[0].value
+      // const countStatus = map.get(status)
       const stories = map.get("Stories")
       const storyPoints = map.get("Story Points")
-      map.set(status, !countStatus ? 1 : countStatus + 1)
+      // map.set(status, !countStatus ? 1 : countStatus + 1)
       map.set("Stories", !stories ? 1 : stories + 1)
       map.set("Story Points", !storyPoints ? parseFloat(item.card.customFields[3].value) : storyPoints + parseFloat(item.card.customFields[3].value))
     }
   })
-  // return map
-  return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
+  return map
+  // return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
 }
-
-// function countCards(list, keyGetter) {
-//   // let storyPoints = 0
-//   // let stories = 0
-//   const map = new Map()
-//   list.forEach((item) => {
-//     const key = keyGetter(item)
-//     if (key === "CARD") {
-//       const count = map.get(key)
-//       // stories = stories + 1
-//       // map.set("Stories", stories)
-//       map.set(key, !count ? 1 : count + 1)
-//       // storyPoints = storyPoints + parseFloat(item.card.customFields[3].value)
-//       // map.set("Story Points", storyPoints)
-//     }
-//   })
-//   return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
-// }
 
 miro.onReady(() => {
   miro.addListener('SELECTION_UPDATED', (e) => {
